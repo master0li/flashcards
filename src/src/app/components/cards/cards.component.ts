eef074456edf6f8670b9f0fd755736712968d713
Charles Li <cli@ift-aft.org>
2018-11-08 22:35:56 -0600

    update to angular7

--- src/src/app/components/cards/cards.component.ts
+++ src/src/app/components/cards/cards.component.ts
@@ -9,6 +9,8 @@ import { Card } from '../../card';
 export class CardsComponent implements OnInit {
 
   currentCard = new Card(); 
+  currentIndex = 0;
+
   cards = [
 
     new Card('A','A'),
@@ -17,6 +19,7 @@ export class CardsComponent implements OnInit {
     new Card('I','I'),
     new Card('M','M'),
     new Card('N','N'),
+    new Card('P','P'),
 
     new Card('And','And'),
     new Card('Are','Are'),
@@ -29,31 +32,62 @@ export class CardsComponent implements OnInit {
     new Card('The','The'),
     new Card('There','There'),
     new Card('To','To'),
-    new Card('You','You')
+    new Card('You','You'),    
+    new Card('On','On')
   ]
 
   constructor() { }
 
   ngOnInit() {
-    this.currentCard = this.cards[this.randomIntFromInterval(0,this.cards.length - 1)];
-    this.currentCard.Question = this.setCase(this.currentCard.Question);
+
+    this.currentIndex = -1;
+    this.nextCard();
+    
   }
 
   public nextCard()
   {
-    this.currentCard = this.cards[this.randomIntFromInterval(0,this.cards.length - 1)];
+    if (this.currentIndex < 0)
+    {
+      this.shuffleInPlace<Card>(this.cards);
+      this.currentIndex = this.cards.length - 1;
+    }
+
+
+    this.currentCard = { ...this.cards[this.currentIndex] };
     this.currentCard.Question = this.setCase(this.currentCard.Question);
 
+    this.currentIndex--;
+
+  }
+
+  public shuffleInPlace<T>(array: T[]): T[] {
+    // if it's 1 or 0 items, just return
+    if (array.length <= 1) return array;
+  
+    // For each index in array
+    for (let i = 0; i < array.length; i++) {
+  
+      // choose a random not-yet-placed item to place there
+      // must be an item AFTER the current item, because the stuff
+      // before has all already been placed
+      const randomChoiceIndex = this.randomIntFromInterval(i, array.length - 1);
+  
+      // place our random choice in the spot by swapping
+      [array[i], array[randomChoiceIndex]] = [array[randomChoiceIndex], array[i]];
+    }
+  
+    return array;
   }
 
   setCase(word: string)
   {
     let rand = this.randomIntFromInterval(0,2);
 
-    if (rand == 1) {
+    if (rand == 0) {
       word = word.toUpperCase();
     }
-    else{
+    else if (rand == 1) {
       word = word.toLowerCase();
     }
 
